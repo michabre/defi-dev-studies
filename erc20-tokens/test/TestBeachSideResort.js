49f3076b6e94fa22ca6c1ffda18d6d5c89de2e9f
Michael <michaelcbreuer@gmail.com>
2021-12-09 10:30:32 +0800

    Writing tests and cleanup

--- erc20-tokens/test/TestBeachSideResort.js
+++ erc20-tokens/test/TestBeachSideResort.js
@@ -3,6 +3,7 @@ const TestLamboToken = artifacts.require("LamboToken");
 
 let testBeachSideResortContract;
 let lamboToken;
+let owner, user1, user2, other;
 
 contract("TestLamboToken", () => {
   it("contract has been deployed", async () => {
@@ -12,7 +13,7 @@ contract("TestLamboToken", () => {
   });
 });
 
-contract("TestBeachSideResort", function (/* accounts */) {
+contract("TestBeachSideResort", function (accounts) {
 
   it("Contract has been deployed.", async () => {
     await TestBeachSideResort.deployed(lamboToken);
@@ -21,5 +22,39 @@ contract("TestBeachSideResort", function (/* accounts */) {
 
   beforeEach(async () => {
     testBeachSideResortContract = await TestBeachSideResort.new(lamboToken);
+    //accounts
+    owner = accounts[0];
+    user1 = accounts[1];
+    user2 = accounts[2];
+    other = accounts[3];
   });
+
+  it("The token name should be: Lambo Token", async () => {
+    const name = await testBeachSideResortContract.getTokenName();
+    return assert.equal(name, "Lambo Token");
+  });
+
+  it("The token symbol should be: LAM", async () => {
+    const symbol = await testBeachSideResortContract.getTokenSymbol();
+    return assert.equal(symbol, "LAM");
+  });
+
+  it("The number of decimals should be: 18", async () => {
+    const decimals = await testBeachSideResortContract.getTokenDecimals();
+    return assert.equal(decimals.toNumber(), 18);
+  });
+
+  it("The owner should have 1,000,000 Lambo Tokens.", async () => {
+    const balance = await testBeachSideResortContract.getBalanceOf(owner);
+    return assert.equal(balance.toNumber(), 1000000);
+  });
+
+  it("The current total supply should be 1,000,000.", async () => {
+    const supply = await testBeachSideResortContract.getTokenTotalSupply();
+    return assert.equal(supply.toNumber(), 1000000);
+  });
+
+
 });
+
+// 63 961 640 5010
